7eeb40c8fc7bdbc0b6fe01a10608e6f81e09fb41
Shubham Dixit <dixit1998.sd@gmail.com>
2023-09-30 15:09:17 +0530

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,18 @@
 export USE_SSH=true
 export GIT_USER=dixit-shubham-404
 export GIT_PASS=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
+export CURRENT_BRANCH=master
+echo $USE_SSH
+echo $GIT_USER
+echo $GIT_USER
+
+sleep 5
+git checkout deployment
+
+git branch
+sleep 5
+
+yarn deploy
+sleep 5
+
+git checkout master
