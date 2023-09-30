84fe5199d32fe86c24d427d71fef0078f7c49336
Shubham Dixit <dixit1998.sd@gmail.com>
2023-09-30 14:50:32 +0530

    deploymeent

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://dixit-shubham-404.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -21,8 +21,10 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'dixit-shubham-404', // Usually your GitHub org/user name.
   projectName: 'notes', // Usually your repo name.
+  deploymentBranch: 'deployment',
+  trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
