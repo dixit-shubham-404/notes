d19fd760d8352ad257fe679014485bc1b32c9adb
Shubham Dixit <dixit1998.sd@gmail.com>
2023-10-02 21:42:08 +0530

    struct

---
+++ sidebar/Languages/rust.js
@@ -0,0 +1,24 @@
+
+const ownership = {
+    label:'Ownership',
+    type:'doc',
+    id:'Languages/RUST/Ownership'
+  };
+
+const loops = {
+    label:'Loops',
+    type:'doc',
+    id:'Languages/RUST/Loops'
+};
+
+const struct = {
+    label:'Struct',
+    type:'doc',
+    id:'Languages/RUST/Struct'
+};
+
+module.exports = {
+    ownership,
+    loops,
+    struct
+};
